3b8ad2d0d7bc2be9b300138ec5e4819e3608078d
DESKTOP-HKS2RA9\limkimchheng <leomkimchheng14@kit.edu.kh>
2017-10-20 12:43:22 +0700

    config

--- src/main/java/com/Config/SecurityConfig.java
+++ src/main/java/com/Config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
    
     };
 	private static final String[] PUBLIC_MATCHERS = {
-
+        "/",
 		"/profile/**",
 		"/updateTask**",
 		"/updateTaskDetail/**",
